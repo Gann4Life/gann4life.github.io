3e04b4865782151eb30c156a26580542dcf7cb82
mrgann@ubuntu <santiago_ariel_mansilla@hotmail.com>
2022-08-01 06:09:28 -0300

    Decoupled json fetching

--- src/App.js
+++ src/App.js
@@ -3,51 +3,48 @@ import Header from "./components/Header"
 import Portfolio from "./components/Portfolio"
 
 const App = () => {
-  const [projects, setProjects] = useState([])
-  const [information, setInformation] = useState([])
-  
-  useEffect(() => {
-    const getProjects = async () => {
-      const projectsFromServer = await fetchProjects()
-      setProjects(projectsFromServer)
-    }
-    const getInformation = async() => {
-      const informationFromServer = await fetchInformation()
-      setInformation(informationFromServer)
-    }
+	const [projects, setProjects] = useState([])
+	const [information, setInformation] = useState([])
+	
+	useEffect(() => {
+		const getProjects = async () => {
+			const projectsFromServer = await fetchProjects()
+			setProjects(projectsFromServer)
+		}
+		const getInformation = async() => {
+			const informationFromServer = await fetchInformation()
+			setInformation(informationFromServer)
+		}
 
-    getProjects()
-    getInformation()
-  }, [])
+		getProjects()
+		getInformation()
+	}, [])
 
-  // Fetch projects from CodersRank
-  const fetchProjects = async() => {
-    const url = "https://api.codersrank.io/v2/users/gann4life/projects?get_by=username"
-    const res = await fetch(url)
-    const data = await res.json()
-    return data.projects
-  }
+	// Fetch projects from CodersRank
+	const fetchProjects = async() => {
+		const data = await fetchJson("https://api.codersrank.io/v2/users/gann4life/projects?get_by=username")
+		return data.projects
+	}
 
-  // FETCHING CAN BE MADE INTO A SINGLE FUNCTION.
-  // I'M WAY TOO LAZY FOR THAT.
+	// Fetch personal information
+	const fetchInformation = async() => await fetchJson("https://api.codersrank.io/v2/users/gann4life?get_by=username")
 
-  // Fetch personal information
-  const fetchInformation = async() => {
-    const url = "https://api.codersrank.io/v2/users/gann4life?get_by=username"
-    const res = await fetch(url)
-    const data = await res.json()
-    return data
-  }
+	// Returns an object from URL
+	const fetchJson = async(url) => {
+		const res = await fetch(url)
+		//const data = await res.json()
+		return await res.json()
+	}
 
-  return (
-      <div className="app container-fluid py-1 text-center">
-          <div className="container my-5">
-            <Header data={information}/>
-            <hr></hr>
-            {projects.length > 0 ? <Portfolio projects={projects}/> : ""}
-          </div>
-      </div>
-  );
+	return (
+			<div className="app container-fluid py-1 text-center">
+					<div className="container my-5">
+						<Header data={information}/>
+						<hr></hr>
+						{projects.length > 0 ? <Portfolio projects={projects}/> : ""}
+					</div>
+			</div>
+	);
 }
 
 export default App;
