3c205575232780d1d62b8d27a535802a6eb64a14
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2023-02-16 07:40:48 -0300

    Added searchbar
    Filters content based on title and description.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
 import Project from "./Project"
 
 const Portfolio = (props) => {
+    const [getSearchTerm, setSearchTerm] = useState("")
     return(
         <div className="row portfolio justify-content-center">
-            {props.projects.map((project, index) => (
+            <input class="rounded-pill bg-dark text-white border-0 fs-4" type="text" placeholder="Search..." onChange={(event) => setSearchTerm(event.target.value)}/>
+            {props.projects.filter((project) => {
+                const noSearchField = getSearchTerm === ""
+                const titleMatch = project.project_title.toLowerCase().includes(getSearchTerm.toLowerCase())
+                const descriptionMatch = project.description.toLowerCase().includes(getSearchTerm.toLowerCase())
+                const anythingMatches = noSearchField || titleMatch || descriptionMatch
+                return anythingMatches ? project : ""
+            }).map((project, index) => (
                 <Project key={index} data={project}/>
             ))}
         </div>
